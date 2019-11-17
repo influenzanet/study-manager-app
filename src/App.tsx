f42f8cceb2015dd57a9d51c6c184f5812e2cf385
phev8 <phev8@users.noreply.github.com>
2019-11-17 21:09:52 +0100

    add libraries that are used by the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import {
+  createMuiTheme,
+  StylesProvider,
+  MuiThemeProvider,
+} from '@material-ui/core';
+
+import Button from '@material-ui/core/Button';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 const App: React.FC = () => {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: '#475d9d',
+        dark: '#000e42',
+        main: '#0f346e',
+      },
+      secondary: {
+        light: '#93e3e2',
+        dark: '#2d8181',
+        main: '#61b1b0',
+        contrastText: '#ffffff',
+      },
+      background: {
+        default: '#f2f2f2',
+      }
+    },
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <StylesProvider injectFirst>
+        <Router>
+
+          {/* The rest of your application */}
+          <h1>Test</h1>
+          <Button variant="contained">
+            Default
+          </Button>
+          <Button variant="contained" color="primary">
+            Default
+          </Button>
+          <Button variant="contained" color="secondary">
+            Default
+          </Button>
+
+        </Router>
+      </StylesProvider>
+    </MuiThemeProvider>
+
   );
 }
 
