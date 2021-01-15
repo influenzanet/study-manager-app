4965e3686a999fbb4fbe1f550ddfadf0aa77d12f
LisaHermans <71076574+LisaHermans@users.noreply.github.com>
2021-01-15 10:29:13 +0100

    Update inf-weekly.ts

--- src/editor-example-generators/belgium/inf-weekly.ts
+++ src/editor-example-generators/belgium/inf-weekly.ts
@@ -24,6 +24,7 @@ const weekly = (): Survey | undefined => {
         new Map([
             ["en", "Survey about your health status in the last week."],
             ["nl", "Klik hier voor je vragenlijst over je klachten in de afgelopen week. Meld alsjeblieft ook als je geen klachten had."],
+            ["nl-be", "Klik hier voor je vragenlijst over je klachten in de afgelopen week. Meld alsjeblieft ook als je geen klachten had."],
         ])
     ));
 
@@ -31,6 +32,7 @@ const weekly = (): Survey | undefined => {
         new Map([
             ["en", "15 seconds to 3 minutes, depending on your symptoms."],
             ["nl", "Invullen duurt 15 seconden tot 3 minuten, afhankelijk van je klachten."],
+            ["nl-be", "Invullen duurt 15 seconden tot 3 minuten, afhankelijk van je klachten."],
         ])
     ));
 
