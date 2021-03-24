e7c4ca5f0fe61230fce5306439071811e40705e3
Kevin Brendel <kBrendel@hotmail.de>
2021-03-24 20:36:09 +0100

    Added all CovidTest questions in new format.

--- src/editor-example-generators/nl/long-covid/questions/covidTest.ts
+++ src/editor-example-generators/nl/long-covid/questions/covidTest.ts
@@ -7,26 +7,35 @@ import { GroupItemEditor } from "../../../../editor-engine/utils/survey-group-ed
 export class CovidTestGroup extends GroupItemEditor {
 
     constructor(parentKey: string, keyOverride?: string) {
-        const groupKey = keyOverride ? keyOverride : 'CT';
+        const groupKey = keyOverride ? keyOverride : 'TEST';
         super(parentKey, groupKey);
         this.initQuestions();
     }
 
     initQuestions() {
-        const L1q1 = q_L1q1_def(this.key, true);
-        const condition_L1q1_yes = CommonExpressions.singleChoiceOptionsSelected(L1q1.key, '0');
+        const test = q_test_def(this.key, true);
+        const condition_test_yes = CommonExpressions.singleChoiceOptionsSelected(test.key, 'yes');
+        const test_result = q_test_result_def(this.key, true, condition_test_yes);
+        const condition_test_result_pos = CommonExpressions.singleChoiceOptionsSelected(test_result.key, 'pos');
+        const infect_earlier = q_infect_earlier_def(this.key, true);
+        const condition_pos_earl_test = CommonExpressions.singleChoiceOptionsSelected(infect_earlier.key, 'pos_earl_test');
+        const condition_pos_earl_notest = CommonExpressions.singleChoiceOptionsSelected(infect_earlier.key, 'pos_earl_notest');
 
-        this.addItem(L1q1);
-        this.addItem(q_C1q2_def(this.key, true, condition_L1q1_yes));
-        this.addItem(q_I1q3_def(this.key, true, condition_L1q1_yes));
-        this.addItem(q_C1q4_def(this.key, true, condition_L1q1_yes));
-        this.addItem(q_L1q5_def(this.key, true, condition_L1q1_yes));
-        //TODO: Add following questions.
+        this.addItem(test);
+        this.addItem(q_test_date_def(this.key, true, condition_test_yes));
+        this.addItem(q_test_location_def(this.key, true, condition_test_yes));
+        this.addItem(q_test_reason_def(this.key, true, condition_test_yes));
+        this.addItem(test_result);
+        this.addItem(q_test_type_def(this.key, true, condition_test_result_pos));
+        this.addItem(infect_earlier);
+        this.addItem(q_inf_earlier_type_def(this.key, true, condition_pos_earl_test));
+        this.addItem(q_inf_earlier_testdate_def(this.key, true, condition_pos_earl_test));
+        this.addItem(q_inf_earlier_date_def(this.key, true, condition_pos_earl_notest));
     }
 }
 
-const q_L1q1_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
-    const itemKey = keyOverride ? keyOverride : 'L1q1';
+const q_test_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q1';
 
     return QuestionGenerators.singleChoice({
         parentKey: parentKey,
@@ -37,13 +46,13 @@ const q_L1q1_def = (parentKey: string, isRequired?: boolean, condition?: Express
         ]),
         responseOptions: [
             {
-                key: '0', role: 'option',
+                key: 'yes', role: 'option',
                 content: new Map([
                     ["nl", "Ja"],
                 ])
             },
             {
-                key: '1', role: 'option',
+                key: 'no', role: 'option',
                 content: new Map([
                     ["nl", "Nee"],
                 ])
@@ -53,8 +62,8 @@ const q_L1q1_def = (parentKey: string, isRequired?: boolean, condition?: Express
     });
 }
 
-const q_C1q2_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
-    const itemKey = keyOverride ? keyOverride : 'C1q2';
+const q_test_date_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q2';
 
     return QuestionGenerators.dateInput({
         parentKey: parentKey,
@@ -73,8 +82,8 @@ const q_C1q2_def = (parentKey: string, isRequired?: boolean, condition?: Express
     });
 }
 
-const q_I1q3_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
-    const itemKey = keyOverride ? keyOverride : 'I1q3';
+const q_test_location_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q3';
 
     return QuestionGenerators.singleChoice({
         parentKey: parentKey,
@@ -137,8 +146,8 @@ const q_I1q3_def = (parentKey: string, isRequired?: boolean, condition?: Express
     });
 }
 
-const q_C1q4_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
-    const itemKey = keyOverride ? keyOverride : 'C1q4';
+const q_test_reason_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q4';
 
     return QuestionGenerators.multipleChoice({
         parentKey: parentKey,
@@ -190,8 +199,8 @@ const q_C1q4_def = (parentKey: string, isRequired?: boolean, condition?: Express
     });
 }
 
-const q_L1q5_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
-    const itemKey = keyOverride ? keyOverride : 'L1q5';
+const q_test_result_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q5';
 
     return QuestionGenerators.singleChoice({
         parentKey: parentKey,
@@ -202,30 +211,166 @@ const q_L1q5_def = (parentKey: string, isRequired?: boolean, condition?: Express
         ]),
         responseOptions: [
             {
-                key: '0', role: 'option',
+                key: 'pos', role: 'option',
                 content: new Map([
-                    ["nl", "Positief, dus WEL besmet (geweest) met het coronavirus"],
+                    ["nl", "Positief, dus WEL besmet (geweest) met het coronavirus"],
                 ])
             },
             {
-                key: '1', role: 'option',
+                key: 'neg', role: 'option',
                 content: new Map([
-                    ["nl", "Negatief, dus GEEN bewijs voor besmetting met het coronavirus"],
+                    ["nl", "Negatief, dus GEEN bewijs voor besmetting met het coronavirus"],
                 ])
             },
             {
-                key: '2', role: 'option',
+                key: 'unknown', role: 'option',
                 content: new Map([
                     ["nl", "Ik heb de uitslag nog niet"],
                 ])
             },
             {
-                key: '3', role: 'option',
+                key: 'no-rep', role: 'option',
                 content: new Map([
-                    ["nl", "Dit wil ik niet aangeven"],
+                    ["nl", "Dit wil ik niet aangeven"],
                 ])
             },
         ],
+        isRequired: isRequired,
+    });
+}
+
+const q_test_type_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q6';
+
+    return QuestionGenerators.singleChoice({
+        parentKey: parentKey,
+        itemKey: itemKey,
+        condition: condition,
+        questionText: new Map([
+            ["nl", "Q6"],
+        ]),
+        responseOptions: [
+            {
+                key: '0', role: 'option',
+                content: new Map([
+                    ["nl", "0"],
+                ])
+            },
+            {
+                key: '1', role: 'option',
+                content: new Map([
+                    ["nl", "1"],
+                ])
+            },
+        ],
+        isRequired: isRequired,
+    });
+}
+
+const q_infect_earlier_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q7';
+
+    return QuestionGenerators.singleChoice({
+        parentKey: parentKey,
+        itemKey: itemKey,
+        condition: condition,
+        questionText: new Map([
+            ["nl", "Q7"],
+        ]),
+        responseOptions: [
+            {
+                key: 'pos_earl_test', role: 'option',
+                content: new Map([
+                    ["nl", "Ja, bevestigd met een positieve test"],
+                ])
+            },
+            {
+                key: 'pos_earl_notest', role: 'option',
+                content: new Map([
+                    ["nl", "Ja, maar er is geen test gedaan"],
+                ])
+            },
+            {
+                key: 'no', role: 'option',
+                content: new Map([
+                    ["nl", "Nee, ik denk het niet"],
+                ])
+            },
+            {
+                key: 'unknown', role: 'option',
+                content: new Map([
+                    ["nl", "Weet ik niet"],
+                ])
+            },
+        ],
+        isRequired: isRequired,
+    });
+}
+
+const q_inf_earlier_type_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q8';
+
+    return QuestionGenerators.singleChoice({
+        parentKey: parentKey,
+        itemKey: itemKey,
+        condition: condition,
+        questionText: new Map([
+            ["nl", "Q8"],
+        ]),
+        responseOptions: [
+            {
+                key: '0', role: 'option',
+                content: new Map([
+                    ["nl", "0"],
+                ])
+            },
+            {
+                key: '1', role: 'option',
+                content: new Map([
+                    ["nl", "1"],
+                ])
+            },
+        ],
+        isRequired: isRequired,
+    });
+}
+
+const q_inf_earlier_testdate_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q9';
+
+    return QuestionGenerators.dateInput({
+        parentKey: parentKey,
+        itemKey: itemKey,
+        condition: condition,
+        questionText: new Map([
+            ["nl", "Q9"],
+        ]),
+        dateInputMode: 'YMD',
+        placeholderText: new Map([
+            ["nl", "dd-mm-jjjj"],
+        ]),
+        minRelativeDate: undefined,
+        maxRelativeDate: undefined,
+        isRequired: isRequired,
+    });
+}
+
+const q_inf_earlier_date_def = (parentKey: string, isRequired?: boolean, condition?: Expression, keyOverride?: string): SurveyItem => {
+    const itemKey = keyOverride ? keyOverride : 'Q10';
+
+    return QuestionGenerators.dateInput({
+        parentKey: parentKey,
+        itemKey: itemKey,
+        condition: condition,
+        questionText: new Map([
+            ["nl", "Q10"],
+        ]),
+        dateInputMode: 'YMD',
+        placeholderText: new Map([
+            ["nl", "dd-mm-jjjj"],
+        ]),
+        minRelativeDate: undefined,
+        maxRelativeDate: undefined,
         isRequired: isRequired,
     });
 }
