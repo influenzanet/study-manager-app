9707961b885bb1e21fe231e22713c98457d9f875
phev8 <phev8@users.noreply.github.com>
2021-04-11 12:51:43 +0200

    continued email rules - not finished yet

--- src/editor-example-generators/nl/long-covid/emailRules.ts
+++ src/editor-example-generators/nl/long-covid/emailRules.ts
@@ -1,14 +1,17 @@
+import { Expression } from "survey-engine/lib/data_types"
 import { StudyExpressions } from "../../../editor-engine/utils/commonExpressions"
+import { durationObjectToSeconds } from "../../../editor-engine/utils/duration"
 import { expWithArgs } from "../../../editor-engine/utils/simple-generators"
+import { surveyKeys } from "./studyRules"
 
 const reminderCondition = (surveyKey: string, activeForDays: number) => {
-    expWithArgs('and',
-        StudyExpressions.hasSurveyKeyAssigned(surveyKey),
+    return expWithArgs('and',
+        StudyExpressions.hasSurveyKeyActive(surveyKey),
         expWithArgs('not',
             StudyExpressions.hasSurveyKeyValidFromOlderThan(surveyKey, {
                 days: -activeForDays
             })
-        )
+        ),
     )
 }
 
@@ -24,3 +27,48 @@ const surveyActiveForMax = {
     T0:  3.5,
     short:  2.5,
 }*/
+
+
+
+const generateStudyReminderEmailConfig = (
+    studyKey: string,
+    label: string,
+    firstTime: Date,
+    period: Duration,
+    defaultLanguage: string,
+    condition: Expression,
+    translations: Array<{
+        lang: string,
+        subject: string,
+    }>
+) => {
+    return {
+        sendTo: 'study-participants',
+        studyKey: studyKey,
+        messageType: 'study-reminder',
+        condition: { dtype: 'exp', exp: condition },
+        nextTime: Math.floor(firstTime.getTime() / 1000.0),
+        period: Math.floor(durationObjectToSeconds(period)),
+        defaultLaunguage: defaultLanguage,
+        translations: translations,
+    }
+}
+
+const studyKey = 'longcovid';
+const defaultLanguage = 'nl';
+const firstTime = new Date(2021, 4, 8, 12, 0, 0);
+
+const emailConfigs = {
+    T0Reminder: generateStudyReminderEmailConfig(
+        studyKey,
+        'T0 reminder',
+        firstTime,
+        { days: 1 },
+        defaultLanguage,
+        reminderCondition(surveyKeys.T0, 3.8),
+        [
+            { lang: 'nl', subject: 'Your first survey is available' }
+        ],
+    ),
+    // TshortReminder: ...
+}
