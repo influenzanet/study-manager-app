bf1e4ef3e8974ace4764b03fc4fe96e5180adb3f
phev8 <phev8@users.noreply.github.com>
2021-06-17 10:20:35 +0200

    eq5d copyrights and pagebreaks

--- src/editor-example-generators/nl/long-covid/questions/for-children/symptoms.ts
+++ src/editor-example-generators/nl/long-covid/questions/for-children/symptoms.ts
@@ -1,4 +1,4 @@
-import { Expression, SurveyItem } from "survey-engine/lib/data_types";
+import { Expression } from "survey-engine/lib/data_types";
 import { CommonExpressions } from "../../../../../editor-engine/utils/commonExpressions";
 import { ComponentGenerators } from "../../../../../editor-engine/utils/componentGenerators";
 import { SurveyItemGenerators } from "../../../../../editor-engine/utils/question-type-generator";
