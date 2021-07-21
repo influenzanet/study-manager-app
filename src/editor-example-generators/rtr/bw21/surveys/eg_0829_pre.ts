bf81651f40da07f92c4965e006ce1075b1ebb968
phev8 <phev8@users.noreply.github.com>
2021-07-21 11:48:14 +0200

    add example question rename RTR surveys - one per event

--- src/editor-example-generators/rtr/bw21/surveys/eg_0829_pre.ts
+++ src/editor-example-generators/rtr/bw21/surveys/eg_0829_pre.ts
@@ -1,5 +1,6 @@
 import { Survey } from "survey-engine/lib/data_types";
 import { SurveyItemGenerators } from "../../../../editor-engine/utils/question-type-generator";
+import { generateLocStrings } from "../../../../editor-engine/utils/simple-generators";
 import { SimpleSurveyEditor } from "../../../../editor-engine/utils/simple-survey-editor";
 import { surveyKeys } from "../studyRules";
 
@@ -19,10 +20,81 @@ export const generate_EG0829PRE = (): Survey | undefined => {
         ])
     })
 
+    // TODO: add questions
+    surveyEditor.addSurveyItemToRoot(Q1(surveyKey));
+
     // Survey End
     surveyEditor.addSurveyItemToRoot(SurveyItemGenerators.surveyEnd(surveyKey, new Map([
         ['de', 'TODO: Text for end of survey']
     ])));
 
     return surveyEditor.getSurvey();
+}
+
+const Q1 = (parentKey: string) => {
+    return SurveyItemGenerators.simpleLikertGroup({
+        parentKey: parentKey,
+        itemKey: "TODO",
+        isRequired: true,
+        questionText: new Map([
+            ["nl", "Geef aan hoe benauwd / kortademig je jezelf de meeste dagen van de afgelopen maand voelde:"],
+        ]),
+        topDisplayCompoments: [{
+            role: 'text',
+            style: [{ key: 'className', value: 'mb-2' }],
+            content: generateLocStrings(new Map([
+                ["nl", "1 = helemaal niet benauwd/kortademig, 10 = heel erg benauwd/kortademig"],
+            ]))
+        }],
+        scaleOptions: [
+            {
+                key: '1', content: new Map([
+                    ["nl", "1"],
+                ])
+            }, {
+                key: '2', content: new Map([
+                    ["nl", "2"],
+                ])
+            }, {
+                key: '3', content: new Map([
+                    ["de", "3"],
+                ])
+            }, {
+                key: '4', content: new Map([
+                    ["nl", "4"],
+                ]),
+            }, {
+                key: '5', content: new Map([
+                    ["nl", "5"],
+                ])
+            }, {
+                key: '6', content: new Map([
+                    ["nl", "6"],
+                ])
+            }, {
+                key: '7', content: new Map([
+                    ["nl", "7"],
+                ])
+            }, {
+                key: '8', content: new Map([
+                    ["nl", "8"],
+                ])
+            }, {
+                key: '9', content: new Map([
+                    ["nl", "9"],
+                ])
+            }, {
+                key: '10', content: new Map([
+                    ["nl", "10"],
+                ])
+            }
+        ],
+        rows: [
+            {
+                key: 'a', content: new Map([
+                    ["nl", "Test test test"],
+                ])
+            },
+        ]
+    });
 }
