ba92bcbd9024dcdaf73249db0d9cd8aa790896a0
Katja Schneider <katja.schneider@coneno.com>
2021-09-02 11:11:22 +0200

    Create kg_0919_post.ts

---
+++ src/editor-example-generators/rtr/bw21/surveys/kg_0919_post.ts
@@ -0,0 +1,68 @@
+import { Survey } from "survey-engine/lib/data_types";
+import { ComponentGenerators } from "../../../../editor-engine/utils/componentGenerators";
+import { SurveyItemGenerators } from "../../../../editor-engine/utils/question-type-generator";
+import { SimpleSurveyEditor } from "../../../../editor-engine/utils/simple-survey-editor";
+import { surveyKeys } from "../studyRules";
+import { getIMAGEAB, getIMAGEAL, getIMAGEOS, getKANZLER, getPERS3, getPERS4, getPROB1, getKPROB1, getPROB2, getKPROB2, getSKPART, getSKPOL, getSWABS, getWABS, getWBT, getWK, getWKINT } from "./question_pool/questions";
+
+
+export const generate_KG0919POST = (): Survey | undefined => {
+    const surveyKey = surveyKeys.KG_0919_POST;
+
+    const surveyEditor = new SimpleSurveyEditor({
+        surveyKey: surveyKey,
+        name: new Map([
+            ["de", "Vragenlijst start LongCOVID-onderzoek"],
+        ]),
+        description: new Map([
+            ["de", "Dit is de eerste vragenlijst van het LongCOVID-onderzoek. De vragenlijst richt zich op je gezondheid, vaccinaties en zorggebruik."],
+        ]),
+        durationText: new Map([
+            ["de", "Invullen van deze vragenlijst kost ongeveer 30 minuten van je tijd."],
+        ])
+    })
+
+    surveyEditor.editor.setMaxItemPerPage({ small: 1, large: 1 });
+
+    const isRequired = true;
+
+    surveyEditor.addSurveyItemToRoot(SurveyItemGenerators.display({
+        parentKey: surveyKey,
+        itemKey: 'intro',
+        content: [ComponentGenerators.markdown({
+            content: new Map([
+                ['de', `
+Im Fragebogen gibt es keine richtigen oder falschen Antworten, sondern nur solche, die Ihrer Perspektive besser oder schlechter entsprechen. Bitte geben Sie deshalb jeweils die Antwort, die Ihrer Ansicht oder Ihrer Situation am nächsten kommt.
+
+Die Umfrage dauert nach unseren Erfahrungen ca. 10 Minuten.
+                `]
+            ]),
+        })],
+    }));
+
+    surveyEditor.addSurveyItemToRoot(getWKINT(surveyKey, isRequired));
+    const WBT = getWBT(surveyKey, isRequired);
+    surveyEditor.addSurveyItemToRoot(WBT);
+    surveyEditor.addSurveyItemToRoot(getWABS(surveyKey, WBT.key, isRequired));
+    surveyEditor.addSurveyItemToRoot(getSWABS(surveyKey, WBT.key, isRequired));
+    surveyEditor.addSurveyItemToRoot(getSKPART(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getSKPOL(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getPROB1(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getKPROB1(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getPROB2(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getKPROB2(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getIMAGEAL(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getIMAGEOS(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getIMAGEAB(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getKANZLER(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getWK(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getPERS3(surveyKey, isRequired));
+    surveyEditor.addSurveyItemToRoot(getPERS4(surveyKey, isRequired));
+
+    // Survey End
+    surveyEditor.addSurveyItemToRoot(SurveyItemGenerators.surveyEnd(surveyKey, new Map([
+        ['de', 'Vielen Dank für Ihre Angaben! Bitte schließen Sie nun die Umfrage ab.']
+    ])));
+
+    return surveyEditor.getSurvey();
+}
