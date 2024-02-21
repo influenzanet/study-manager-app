b2bab1cb2471f1f30c8b703d6b6082edabcb1277
merlinvanloenen <merlin.van.loenen@rivm.nl>
2024-02-21 18:51:29 +0100

    Update medicine.ts
    added a number control for the numerical inputs in the consults qeustions:
    1)gen_Q2a_longsymptoms
    2)gen_Q2a

--- src/editor-example-generators/nl/long-covid/questions/medicine.ts
+++ src/editor-example-generators/nl/long-covid/questions/medicine.ts
@@ -5,6 +5,7 @@ import { SurveyItemGenerators } from "../../../../editor-engine/utils/question-t
 import { generateLocStrings } from "../../../../editor-engine/utils/simple-generators";
 import { GroupItemEditor } from "../../../../editor-engine/utils/survey-group-editor-helper";
 import { surveyKeys } from "../studyRules";
+import { checkIfOpenNumberFieldIsAnsweredForMC, checkIfOpenTextFieldIsAnsweredForMultipleChoice, checkIfOpenTextFieldIsAnsweredForSingleChoice } from "./utils";
 
 export class MedicineGroup extends GroupItemEditor {
 
@@ -109,6 +110,13 @@ const gen_Q2a_longsymptoms = (parentKey: string, isRequired?: boolean, condition
         questionText: new Map([
             ["nl", "Met welke zorgverleners heb je contact gehad in de afgelopen 3 maanden voor klachten die WEL te maken hebben met het coronavirus, en hoe vaak? "],
         ]),
+        customValidations: [
+            checkIfOpenNumberFieldIsAnsweredForMC(
+                `${parentKey}.${itemKey}`,
+                //['0', '1', '2'] // list here all the options that have an open field
+                ['huisarts', '0', 'specialist', '1', '2', '4', '7', '10', '11', 'longarts', '14', '16', '17', '18', '20', '21', '22', '23', 'paraspecialist', '3', '5', '6', '15', '12', 'overigespec', '8', '13', '19']
+            )
+        ],
         responseOptions: [
             {
                 key: 'huisarts', role: 'text',
@@ -393,6 +401,17 @@ const gen_Q2a = (parentKey: string, isRequired?: boolean, condition?: Expression
         questionText: new Map([
             ["nl", "Met welke zorgverleners heb je contact gehad in de afgelopen 3 maanden voor klachten die NIET te maken hebben met het coronavirus?"],
         ]),
+        customValidations: [
+            checkIfOpenNumberFieldIsAnsweredForMC(
+                `${parentKey}.${itemKey}`,
+                //['0', '1', '2'] // list here all the options that have an open field
+                ['huisarts', '0', 'specialist', '1', '2', '4', '7', '10', '11', 'longarts', '14', '16', '17', '18', '20', '21', '22', '23', 'paraspecialist', '3', '5', '6', '15', '12', 'overigespec', '8', '13', '19']
+            //),
+            //checkIfOpenTextFieldIsAnsweredForMultipleChoice(
+            //    `${parentKey}.${itemKey}`,
+            //    ['24']
+            )
+        ],
         responseOptions: [
             {
                 key: 'huisarts', role: 'text',
