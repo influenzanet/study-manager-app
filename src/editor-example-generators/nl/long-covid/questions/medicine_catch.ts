9e753918b7693a2f164c0ec4cf038f2e8efbbc84
merlinvanloenen <merlin.van.loenen@rivm.nl>
2024-03-13 17:34:34 +0100

    Update medicine_catch.ts
    made the maximun number of seeing a healthcare worker higher- to 9999 times.

--- src/editor-example-generators/nl/long-covid/questions/medicine_catch.ts
+++ src/editor-example-generators/nl/long-covid/questions/medicine_catch.ts
@@ -97,7 +97,7 @@ const gen_Q2a_longsymptoms = (parentKey: string, isRequired?: boolean, condition
 
     const inputProperties = {
         min: 1,
-        max: 50
+        max: 9999
     };
     const inputStyle = [{ key: 'inputMaxWidth', value: '70px' }];
 
@@ -381,7 +381,7 @@ const gen_Q2a = (parentKey: string, isRequired?: boolean, condition?: Expression
 
     const inputProperties = {
         min: 1,
-        max: 50
+        max: 9999
     };
     const inputStyle = [{ key: 'inputMaxWidth', value: '70px' }];
 
